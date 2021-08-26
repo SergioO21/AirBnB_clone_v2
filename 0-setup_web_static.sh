51ea397ad11d17944e27ef27991152ce6483f6fa
Sergio Orejarena <sergioorueda@gmail.com>
2021-08-26 17:03:40 -0500

    Without if

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 # Sets up your web servers for the deployment of web_static.
 
-if [ ! -x "$(command -v nginx)" ]
-then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
+sudo apt-get -y update
+sudo apt-get -y install nginx
 
 sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 
