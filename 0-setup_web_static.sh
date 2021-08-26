cb1a2fa5d383e5fab3bb48d3c9c1de135575ba28
Sergio Orejarena <sergioorueda@gmail.com>
2021-08-26 17:12:55 -0500

    Other fucking form

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,11 +12,6 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-add="\\\n\
-        location /hbnb_static {\\
-            alias /data/web_static/current/;\\
-        }"
-
-sudo sed -i "/server_name _;/a $add" /etc/nginx/sites-available/default
+sudo sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
 
 sudo service nginx restart
