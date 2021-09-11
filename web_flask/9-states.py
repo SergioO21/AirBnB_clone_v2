089cda3e796cfaea5d0f63fa726dc762ff8a926a
Sergio Orejarena <sergioorueda@gmail.com>
2021-09-11 00:10:54 -0500

    States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id=None):
+    """ Display a HTML page: (inside the tag BODY) """
+    states = list(storage.all(State).values())
+    _id = id
+
+    if id:
+        for state in states:
+            if state.id == id:
+                state_id = state
+                break
+    else:
+        state_id = states
+
+    return render_template("9-states.html", **locals())
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
